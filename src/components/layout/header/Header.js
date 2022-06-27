6573cb0d56e14d4c6ad18cdd9b48b6d44bd82a74
Ankush <ankush357159@gmail.com>
2022-06-27 12:14:57 +0530

    development

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -7,14 +7,13 @@ import {
   ThemeProvider,
   Toolbar,
 } from "@mui/material";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import DeveloperBoardIcon from "@mui/icons-material/DeveloperBoard";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import CardMedia from "@mui/material/CardMedia";
 import Logo from "../../../assets/Logo.png";
-
 
 const theme = createTheme({
   palette: {
@@ -32,8 +31,11 @@ const Header = () => {
   return (
     <ThemeProvider theme={theme}>
       <AppBar
-        
-        sx={{position:'relative', backgroundColor: "#0a132b", verticalAlign: "center" }}
+        sx={{
+          position: "relative",
+          backgroundColor: "#0a132b",
+          verticalAlign: "center",
+        }}
       >
         <Toolbar>
           <Box
@@ -45,17 +47,17 @@ const Header = () => {
             }}
           >
             <Link to='/'>
-            <Box sx={{ display: "flex", flexGrow: 0.1 }}>
-              <CardMedia
-                component='img'
-                image={Logo}
-                height='50px'
-                sx={{
-                  objectFit: "scale-down",
-                  cursor: "pointer",
-                }}
-              />
-            </Box>
+              <Box sx={{ display: "flex", flexGrow: 0.1 }}>
+                <CardMedia
+                  component='img'
+                  image={Logo}
+                  height='50px'
+                  sx={{
+                    objectFit: "scale-down",
+                    cursor: "pointer",
+                  }}
+                />
+              </Box>
             </Link>
             <Box
               sx={{
@@ -65,23 +67,37 @@ const Header = () => {
                 flexGrow: 0.9,
               }}
             >
-              
-              <Button startIcon={<HomeIcon />} sx={{ px: 3, cursor: 'pointer',  }} component={Link} to={'/'}>
+              <Button
+                startIcon={<HomeIcon />}
+                sx={{ px: 3, cursor: "pointer" }}
+                component={Link}
+                to={"/"}
+              >
                 Home
               </Button>
-              
-              
-              <Button startIcon={<PersonIcon />} sx={{ px: 3 , cursor: 'pointer'}} component={Link} to={'/about'}>
+
+              <Button
+                startIcon={<PersonIcon />}
+                sx={{ px: 3, cursor: "pointer" }}
+                component={Link}
+                to={"/about"}
+              >
                 About
               </Button>
-              
-              <Button startIcon={<DeveloperBoardIcon />} sx={{ px: 3, cursor: 'pointer' }}
-              component={Link} to={'/projects'}>
+
+              <Button
+                startIcon={<DeveloperBoardIcon />}
+                sx={{ px: 3, cursor: "pointer" }}
+                component={Link}
+                to={"/projects"}
+              >
                 Projects
               </Button>
-              <Button>
-                <GitHubIcon />
-              </Button>
+              <a href='https://github.com/ankush357159/folio'>
+                <Button>
+                  <GitHubIcon />
+                </Button>
+              </a>
             </Box>
           </Box>
         </Toolbar>
